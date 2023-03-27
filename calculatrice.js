9616740a3a4623059a13ce673397a8af0c635901
isaurevico <90193472+isaurevico@users.noreply.github.com>
2023-03-27 23:12:05 +0200

    début étape3

--- calculatrice.js
+++ calculatrice.js
@@ -1,26 +1,33 @@
-let calcul = "";
-let calculAffichage = "";
-let tabChiffres = ["zero","un","deux","trois","quatre","cinq","six","sept","huit","neuf"];
-let tabCaracteres = ["diviser", "multiplication","moins", "plus","parenthese1","parenthese2","virgule"];
-let tabSymboles = ["/","*","-","+","(",")","."];
-let tabSymbolesAffichage = [" ÷ "," x "," - "," + ","(",")",","];
+const tabChiffres = ["zero","un","deux","trois","quatre","cinq","six","sept","huit","neuf"];
+const tabCaracteres = ["diviser", "multiplication","moins", "plus","parenthese1","parenthese2","virgule"];
+const tabSymboles = ["/","*","-","+","(",")","."];
+const tabSymbolesAffichage = [" ÷ "," x "," - "," + ","(",")",","];
 
+class Calculatrice{
+    constructor(){
+        this.calcul = "";
+        this.calculAffichage = "";
+    }
 
-for(let i = 0; i < tabChiffres.length; i++){
-    document.getElementById(tabChiffres[i]).addEventListener("click", () =>{
-        calcul += i;
-        calculAffichage += i;
-        document.getElementById("calcul").innerHTML = calculAffichage;
-    });
+    afficheCalcul(element){
+        if(tabSymboles.includes(element)){
+                calcul += tabSymboles[tabSymboles.indexOf(element)];
+                calculAffichage += tabSymbolesAffichage[tabSymbolesAffichage.indexOf(element)];
+                document.getElementById("calcul").innerHTML = calculAffichage;
+            }
+
+        else if(typeof element == "number"){
+            calcul += element;
+            calculAffichage += element;
+            document.getElementById("calcul").innerHTML = calculAffichage;
+        }
+
+        
+    }
 }
 
-for(let i = 0; i < tabSymboles.length; i++){
-    document.getElementById(tabCaracteres[i]).addEventListener("click", () =>{
-        calcul += tabSymboles[i];
-        calculAffichage += tabSymbolesAffichage[i];
-        document.getElementById("calcul").innerHTML = calculAffichage;
-    });
-}
+let calculatrice = new Calculatrice();
+
 
 
 document.getElementById("egal").addEventListener("click", () =>{
