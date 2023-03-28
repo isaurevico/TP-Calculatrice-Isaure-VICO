658c02d50ea39bccdec2c27ac9131ade08732db0
isaurevico <90193472+isaurevico@users.noreply.github.com>
2023-03-28 15:37:09 +0200

    etape3&4 finies !

--- calculatrice.js
+++ calculatrice.js
@@ -5,46 +5,49 @@ const tabSymbolesAffichage = [" ÷ "," x "," - "," + ","(",")",","];
 
 class Calculatrice{
     constructor(){
-        this.calcul = "";
-        this.calculAffichage = "";
+        this.calcul = [];
+        this.calculAffichage = [];
     }
 
     afficheCalcul(element){
+        console.log(tabSymboles.indexOf(element));
         if(tabSymboles.includes(element)){
-                calcul += tabSymboles[tabSymboles.indexOf(element)];
-                calculAffichage += tabSymbolesAffichage[tabSymbolesAffichage.indexOf(element)];
-                document.getElementById("calcul").innerHTML = calculAffichage;
+                this.calcul.push(tabSymboles[tabSymboles.indexOf(element)]);
+                this.calculAffichage.push(tabSymbolesAffichage[tabSymboles.indexOf(element)]);
+                document.getElementById("calcul").innerHTML = this.calculAffichage.join(" ");
             }
 
         else if(typeof element == "number"){
-            calcul += element;
-            calculAffichage += element;
-            document.getElementById("calcul").innerHTML = calculAffichage;
+            this.calcul.push(element);
+            this.calculAffichage.push(element);
+            document.getElementById("calcul").innerHTML = this.calculAffichage.join(" ");
         }
+        console.log(this.calcul)
+    }
 
-        
+    effectuerCalcul(){
+        try{
+            document.getElementById("resultat").innerHTML = eval(this.calcul.join("")); 
+        }
+        catch(e){
+            document.getElementById("resultat").innerHTML = "Erreur de calcul"
+        }
+    }
+
+    resetCalcul(){
+        document.getElementById("calcul").innerHTML = "";
+        document.getElementById("resultat").innerHTML = "";
+        this.calcul = [];
+        this.calculAffichage = [];
+    }
+
+    supprCalcul(){
+        this.calcul.pop();
+        this.calculAffichage.pop();
+        document.getElementById("calcul").innerHTML = this.calculAffichage.join(" ");
     }
 }
 
 let calculatrice = new Calculatrice();
-
-
-
-document.getElementById("egal").addEventListener("click", () =>{
-    try{
-        document.getElementById("resultat").innerHTML = eval(calcul); 
-    }
-    catch(e){
-        document.getElementById("resultat").innerHTML = "Erreur de calcul"
-    }
-});
-
-
-document.getElementById("reset").addEventListener("click", () =>{
-    document.getElementById("calcul").innerHTML = "";
-    document.getElementById("resultat").innerHTML = "";
-    calcul = "";
-    calculAffichage = "";
-});
 
 
